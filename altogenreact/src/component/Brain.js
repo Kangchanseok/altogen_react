56d9633616f7f0c9fe5072de56899d4e3557c376
강찬석 <ssimjunlke@naver.com>
2022-06-08 20:17:24 +0900

    material tableCell css

--- altogenreact/src/component/Brain.js
+++ altogenreact/src/component/Brain.js
@@ -1,21 +1,45 @@
 import dummy from "../db/altogen.json"
+import {Table, TableBody, TableCell, TableHead, TableRow, Button, Typography} from '@material-ui/core'
 
 export default function Brain(){
-// console.log(dummy.Brain.constructor.name)
 return(
     
-    <table className = 'braintest'>
-        {dummy.map((brain,index) =>(
-            <tr key={index}>
-                <td>{brain.title}</td>
-                <br/>
-                <br/>
-                <img src={brain.img} style={{width:'800px', height:'500px'}} ></img>
-                <br/>
-                <td style={{width:'500px', height:'500px', border:'1px solid black'}}>{brain.content}</td>
-            </tr>
+    // <table className = 'braintest'>
+    //     {dummy.map((brain,index) =>(
+    //         <tr key={index}>
+    //             <tr>title
+    //                 <tr>{brain.title}</tr>
+    //             </tr>
+    //             <tr>image
+    //                 <img src={brain.img} style={{width:'700px', height:'500px'}} ></img>
+    //             </tr>
+    //             <tr style={{width:'500px', height:'500px', border:'1px solid black'}}>content
+    //                 <tr>{brain.content}</tr>
+    //             </tr>
+    //         </tr>
                 
-        ))}
-    </table>
+    //     ))}
+    // </table>
+    <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell align="center" style={{fontWeight:'bold', border:'1px solid black', backgroundColor:'black', color:'white'}}>NAME</TableCell>
+              <TableCell align="center" style={{fontWeight:'bold', border:'1px solid black', backgroundColor:'black', color:'white'}}>IMG</TableCell>
+              <TableCell align="center" style={{fontWeight:'bold', border:'1px solid black', backgroundColor:'black', color:'white'}}>CONTENT</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {dummy.map( (brain,index) => (
+              <TableRow key={index}>
+                <TableCell align="center" style={{fontSize:'15px', fontWeight:'bold', border:'1px solid black'}}>{brain.title}</TableCell>
+                <TableCell align="center" style={{border:'1px solid black'}}><img src={brain.img} style={{width:'650px', height:'400px'}}></img></TableCell>
+                <TableCell align="center" style={{fontSize:'17px', border:'1px solid black'}}>{brain.content}</TableCell>
+                
+                
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+
 );
 }
